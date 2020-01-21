f818e84a63eeec456de21683d735d1243a3d10f8
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-01-21 22:49:54 +0100

    vytvoření projektu

---
+++ KasiopeaBanka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KasiopeaBanka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
