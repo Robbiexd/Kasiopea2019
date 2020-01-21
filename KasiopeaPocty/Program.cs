babed3f3cdcbcbd05fa3b7c25ad64ca34f4ef6bd
Robert <48084300+Robbiexd@users.noreply.github.com>
2020-01-21 22:47:31 +0100

    Add project files.

---
+++ KasiopeaPocty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KasiopeaPocty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
